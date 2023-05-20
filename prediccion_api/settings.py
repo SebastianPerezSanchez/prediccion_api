522ff477ff2ab04ccea6f183b61e9289e79a5aee
SebastianPerezSanchez <72109827+SebastianPerezSanchez@users.noreply.github.com>
2023-05-20 02:51:52 -0500

    CORS

--- prediccion_api/settings.py
+++ prediccion_api/settings.py
@@ -39,10 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djongo',
+    'corsheaders',
     'prediccion_app',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
